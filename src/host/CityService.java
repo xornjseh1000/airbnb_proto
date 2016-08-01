b7ebcf90bd0b6504f1cc0775212cafec506172ee
elqqmffntl1 <elqqmffntl@gmail.com>
2016-08-01 16:33:49 +0900

    중간단계...

--- src/host/CityService.java
+++ src/host/CityService.java
@@ -1,9 +1,12 @@
 package host;
 
+import java.util.Map;
+
 public interface CityService {
 	public void regist(CityBean bean);
 	public void update(CityBean bean);
 	public void delete(CityBean bean);
-	public void mapPoint(CityBean bean);
+	public Map<?,?>map();
 	public void review(CityBean bean);
+	public CityBean session();
 }
